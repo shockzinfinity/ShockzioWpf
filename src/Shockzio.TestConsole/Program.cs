45d3c978a518b0e89b24afc89473a74220923241
Jun Yu (RTX) <shockzinfinity@gmail.com>
2023-09-21 18:32:27 +0900

    test: Add Test projects

--- src/Shockzio.TestConsole/Program.cs
+++ src/Shockzio.TestConsole/Program.cs
@@ -1,9 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Shockzio.Design.Geometries;
 using Shockzio.Design.Images;
 
 Console.WriteLine("Hello, World!");
 
 var temp = ImageData.Bmw;
+var temp2 = GeometryData.Minimize;
 
 Console.WriteLine(temp);
 Console.Read();
